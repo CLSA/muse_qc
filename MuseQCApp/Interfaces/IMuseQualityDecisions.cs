6385fcbe44305fccdd5b4a68b354013415afff0d
CodyCooperGit <55212402+CodyCooperGit@users.noreply.github.com>
2023-07-24 14:37:57 -0400

    Added Interface and implementation for Muse Quality Descision

---
+++ MuseQCApp/Interfaces/IMuseQualityDecisions.cs
@@ -0,0 +1,32 @@
+﻿using MuseQCDBAccess.Models;
+
+namespace MuseQCApp.Interfaces;
+
+/// <summary>
+/// An interface for implementing rules to make muse quality decisions
+/// </summary>
+public interface IMuseQualityDecisions
+{
+    /// <summary>
+    /// Determines if the day is an actual day of data.
+    /// NOTE: Sometimes tests are done for small amounts of time to ensure 
+    /// everything is working with the Muse headband. These tests should not 
+    /// be counted as an actual night of data
+    /// </summary>
+    /// <param name="stats">The QC stats to evaluate</param>
+    /// <returns>True if this is an actual night of data, false otherwise</returns>
+    public bool IsActualNight(QCStatsModel stats);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="stats">The QC stats to evaluate</param>
+    /// <returns>True if there is a problem, false otherwise</returns>
+    public bool HasProblem(QCStatsModel stats);
+
+    /// <summary>
+    /// Get the version number of the implementation
+    /// </summary>
+    /// <returns>The version number for the implementation</returns>
+    public int GetVersionNumber();
+}
---
+++ MuseQCApp/Modules/MuseQualityDecisionsV1.cs
@@ -0,0 +1,26 @@
+﻿using MuseQCApp.Interfaces;
+using MuseQCDBAccess.Models;
+
+namespace MuseQCApp.Modules;
+public class MuseQualityDecisionsV1 : IMuseQualityDecisions
+{
+    public int GetVersionNumber()
+    {
+        return 1;
+    }
+
+    public bool HasProblem(QCStatsModel stats)
+    {
+        // Assumptions:
+        // 1. Must be an actual night of data in order to have a problem
+        // 2. A threshold of 0.8 or above is acceptable for FT.any
+        // 3. A duration of 6 hours or above is considered a good night
+        // NOTE: Ft.Any of >= 0.8 is the threshold
+        return IsActualNight(stats) && (stats.FtAny < 0.8 || stats.Dur < 6);
+    }
+
+    public bool IsActualNight(QCStatsModel stats)
+    {
+        return stats.Dur > 1;
+    }
+}
